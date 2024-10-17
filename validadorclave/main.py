72e9d7020f41b2051803aad18a8f270ed8611a2a
JeroK1033 <jeronimohenao4@gmail.com>
2024-10-17 13:34:16 -0500

    Clases organizadas y El main con las excepciones

---
+++ validadorclave/main.py
@@ -0,0 +1,17 @@
+class LongitudInvalida(Exception):
+    pass
+
+class NoContieneMayuscula(Exception):
+    pass
+
+class NoContieneMinuscula(Exception):
+    pass
+
+class NoContieneNumero(Exception):
+    pass
+
+class NoContieneCaracterEspecial(Exception):
+    pass
+
+class NoCumpleCalisto(Exception):
+    pass
--- validadorclave/modelo/validador.py
+++ validadorclave/modelo/validador.py
@@ -1,9 +1,10 @@
 from abc import ABC, abstractmethod
+from main import NoContieneCaracterEspecial, NoContieneMayuscula, NoContieneMinuscula, NoContieneNumero, NoCumpleCalisto
 
 class ReglaValidacion(ABC):
     
     def __init__(self, longitud_esperada: int):
-        self.longitud_esperada: int = longitud_esperada
+        self.longitud_esperada: int = longitud_esperada    
     
     def _validar_longitud(self, clave: str):
         return len(clave) >= self.longitud_esperada
@@ -14,7 +15,7 @@ class ReglaValidacion(ABC):
     def _contiene_minuscula (self, clave: str)-> bool:
         return any(caracter.islower() for caracter in clave)
     
-    def _contiene_mayuscula (self, clave: str)-> bool:
+    def _contiene_numero (self, clave: str)-> bool:
         return any(caracter.isdigit() for caracter in clave)    
     
     @abstractmethod
@@ -22,15 +23,33 @@ class ReglaValidacion(ABC):
         pass
     
     
-class ReglaValidacionCalisto:
+class ReglaValidacionCalisto(ReglaValidacion):
     
     def contiene_calisto(self, clave: str)->bool:
         pass
+    
+    def es_valida(self, clave: str)-> bool:
+        pass
 
-class ReglaValidacionGanimedes:
+
+
+
+class ReglaValidacionGanimedes(ReglaValidacion):
+    
+
     def contiene_caracter_especial(self, clave: str)->bool:
         caracteres_especiales = '@_#$%'
         return any(caracter in clave for caracter in caracteres_especiales)
+    
+    def es_valida(self, clave: str)-> bool:
+        pass
+
+
+
 
 class Validador:
-    pass
+    def __init__(self, regla: ReglaValidacion):
+        pass
+    
+    def es_valida(self, clave: str)-> bool:
+        pass
