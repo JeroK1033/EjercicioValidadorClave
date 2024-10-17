3fc4aa760d8115544fbaf676f5f15c1879108de6
JeroK1033 <jeronimohenao4@gmail.com>
2024-10-17 13:40:12 -0500

    Lo que se udo hacer

--- validadorclave/modelo/validador.py
+++ validadorclave/modelo/validador.py
@@ -1,6 +1,8 @@
 from abc import ABC, abstractmethod
 from main import NoContieneCaracterEspecial, NoContieneMayuscula, NoContieneMinuscula, NoContieneNumero, NoCumpleCalisto
 
+
+
 class ReglaValidacion(ABC):
     
     def __init__(self, longitud_esperada: int):
@@ -23,33 +25,48 @@ class ReglaValidacion(ABC):
         pass
     
     
+    
+    
 class ReglaValidacionCalisto(ReglaValidacion):
     
     def contiene_calisto(self, clave: str)->bool:
         pass
     
     def es_valida(self, clave: str)-> bool:
-        pass
+        self._validar_longitud(clave)
+        self._contiene_mayuscula(clave)
+        self._contiene_minuscula(clave)
+        self._contiene_numero(clave)
+        self.contiene_calisto(clave)
+        return True
+
+
+
 
 
 
 
 class ReglaValidacionGanimedes(ReglaValidacion):
-    
 
     def contiene_caracter_especial(self, clave: str)->bool:
         caracteres_especiales = '@_#$%'
         return any(caracter in clave for caracter in caracteres_especiales)
     
     def es_valida(self, clave: str)-> bool:
-        pass
+        self._validar_longitud(clave)
+        self._contiene_mayuscula(clave)
+        self._contiene_minuscula(clave)
+        self._contiene_numero(clave)
+        self.contiene_caracter_especial(clave)
+        return True
+
 
 
 
 
 class Validador:
     def __init__(self, regla: ReglaValidacion):
-        pass
+        self.regla = regla
     
     def es_valida(self, clave: str)-> bool:
-        pass
+        return self.regla.es_valida(clave)
