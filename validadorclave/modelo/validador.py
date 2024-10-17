aece3ecd1f30f4151f8d3c942897a0b3662ad903
JeroK1033 <jeronimohenao4@gmail.com>
2024-10-17 12:37:43 -0500

    Regla Validaciones a la miad con venv creado

--- validadorclave/modelo/validador.py
+++ validadorclave/modelo/validador.py
@@ -1,1 +1,19 @@
-# TODO: Implementa el código del ejercicio aquí
+from abc import ABC, abstractmethod
+
+class ReglaValidacion(ABC):
+    def __init__(self, longitud_esperada: int):
+        self.longitud_esperada: int = longitud_esperada
+    
+    @abstractmethod
+    def es_valida(self): 
+        pass
+    
+    def _validar_longitud(self, clave) :
+        return len(clave) >= self.longitud_esperada
+
+        
+class ReglaValidacionCalisto:
+    pass
+
+class ReglaValidacionGanimedes:
+    pass
